95e454a7ebaf709102e7cb55fbd242e5a4cdd557
user <ljkdta@gmail.com>
2023-11-16 03:40:25 +0900

    added text recognition to translator

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -153,6 +153,13 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-functions:20.4.0")
     implementation("com.google.code.gson:gson:2.9.0")
+    implementation("com.google.android.gms:play-services-mlkit-text-recognition:19.0.0")
+    implementation("com.google.mlkit:image-labeling:17.0.7")
+    implementation ("com.google.android.gms:play-services-mlkit-text-recognition:19.0.0")
+    implementation ("com.google.android.gms:play-services-mlkit-text-recognition-chinese:16.0.0")
+    implementation ("com.google.android.gms:play-services-mlkit-text-recognition-devanagari:16.0.0")
+    implementation ("com.google.android.gms:play-services-mlkit-text-recognition-japanese:16.0.0")
+    implementation ("com.google.android.gms:play-services-mlkit-text-recognition-korean:16.0.0")
 
     //boardUI
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
