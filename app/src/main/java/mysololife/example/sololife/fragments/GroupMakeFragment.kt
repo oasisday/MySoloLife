8086366785c39ff50e98dfc7074dfb20d18dd763
최형우 <guddn106901@gmail.com>
2023-11-26 23:37:07 +0900

    리스트뷰 추가

--- app/src/main/java/mysololife/example/sololife/fragments/GroupMakeFragment.kt
+++ app/src/main/java/mysololife/example/sololife/fragments/GroupMakeFragment.kt
@@ -44,14 +44,6 @@ class GroupMakeFragment : Fragment() {
 
         binding.boardListView.setOnItemClickListener { parent, view, position, id ->
 
-            // 첫번째 방법으로는 listview에 있는 데이터 title content time 다 다른 액티비티로 전달해줘서 만들기
-//            val intent = Intent(context, BoardInsideActivity::class.java)
-//            intent.putExtra("title", boardDataList[position].title)
-//            intent.putExtra("content", boardDataList[position].content)
-//            intent.putExtra("time", boardDataList[position].time)
-//            startActivity(intent)
-
-            // 두번째 방법으로는 Firebase에 있는 board에 대한 데이터의 id를 기반으로 다시 데이터를 받아오는 방법
             val intent = Intent(context, BoardInsideActivity::class.java)
             intent.putExtra("key", boardKeyList[position])
             startActivity(intent)
@@ -77,10 +69,7 @@ class GroupMakeFragment : Fragment() {
                 boardDataList.clear()
 
                 for (dataModel in dataSnapshot.children) {
-
                     Log.d(TAG, dataModel.toString())
-//                    dataModel.key
-
                     val item = dataModel.getValue(BoardModel::class.java)
                     boardDataList.add(item!!)
                     boardKeyList.add(dataModel.key.toString())
@@ -96,7 +85,6 @@ class GroupMakeFragment : Fragment() {
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
-                // Getting Post failed, log a message
                 Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
             }
         }
--- app/src/main/java/mysololife/example/sololife/fragments/HomeFragment.kt
+++ app/src/main/java/mysololife/example/sololife/fragments/HomeFragment.kt
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -20,9 +22,15 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 import mysololife.example.sololife.CameraActivity
+import mysololife.example.sololife.Matching
 import mysololife.example.sololife.auth.UserDataModel
+import mysololife.example.sololife.group.GroupDataModel
+import mysololife.example.sololife.group.GroupMainActivity
+import mysololife.example.sololife.group.GroupboardLVAdapter
 import mysololife.example.sololife.recorder.RecorderMainActivity
 import mysololife.example.sololife.timetable.TimeTableActivity
+import mysololife.example.sololife.utils.FBAuth
+import mysololife.example.sololife.utils.FBboard
 import mysololife.example.sololife.utils.FirebaseAuthUtils
 import mysololife.example.sololife.utils.FirebaseRef
 
@@ -34,14 +42,81 @@ private const val ARG_PARAM2 = "param2"
 class HomeFragment : Fragment(){
     lateinit var binding: FragmentHomeBinding
     private val uid = FirebaseAuthUtils.getUid()
+    private val boardDataList = mutableListOf<GroupDataModel>()
+    private val boardKeyList = mutableListOf<String>()
+    private lateinit var gboardRVAdapter: GroupboardLVAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+
+        gboardRVAdapter = GroupboardLVAdapter(boardDataList)
+
+        binding.gotoBoardListView.adapter = gboardRVAdapter
+        //클릭시 게시판 이동//
+        binding.gotoBoardListView.setOnItemClickListener { parent, view, position, id->
+            val intent = Intent(context, GroupMainActivity::class.java)
+            intent.putExtra("key", boardKeyList[position])
+            startActivity(intent)
+        }
+
+        /////////////////////////////////////////////
+        binding.matchingBtn.setOnClickListener{
+            val intent = Intent(context, Matching::class.java)
+            startActivity(intent)
+        }
+
+        getFBBoardData()
+
         return binding.root
     }
+
+    private fun getFBBoardData(){
+
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                boardDataList.clear()
+
+                for (dataModel in dataSnapshot.children) {
+
+                    Log.d("homefragment", dataModel.toString())
+//                    dataModel.key
+
+                    val item = dataModel.getValue(GroupDataModel::class.java)
+
+                    if (item != null) {
+                        if(item.member?.contains(FBAuth.getUid()) == true){
+                            boardDataList.add(item!!)
+                            boardKeyList.add(dataModel.key.toString())
+                        }
+                    }
+
+                    //boardDataList.add(item!!)
+                    //boardKeyList.add(dataModel.key.toString())
+
+                }
+
+                boardKeyList.reverse()
+                boardDataList.reverse()
+                gboardRVAdapter.notifyDataSetChanged()
+
+                Log.d("homefragment", boardDataList.toString())
+
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w("homefragment", "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        FBboard.boardInfoRef.addValueEventListener(postListener)
+
+    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
